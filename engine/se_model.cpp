0417656d81bee90368b087bf44758ab6f3007100
MrSplotudo <167779734+MrSplotudo@users.noreply.github.com>
2026-03-28 13:12:54 +0100

    Part 6 complete

---
+++ engine/se_model.cpp
@@ -0,0 +1,60 @@
+#include "se_model.h"
+
+#include <cassert>
+#include <cstring>
+
+namespace se {
+SeModel::SeModel(SeDevice &device, const std::vector<Vertex> &vertices) : seDevice{device} {
+    createVertexBuffers(vertices);
+}
+
+SeModel::~SeModel() {
+    vkDestroyBuffer(seDevice.device(), vertexBuffer, nullptr);
+    vkFreeMemory(seDevice.device(), vertexBufferMemory, nullptr);
+}
+
+void SeModel::createVertexBuffers(const std::vector<Vertex> &vertices) {
+    vertexCount = static_cast<uint32_t>(vertices.size());
+    assert(vertexCount >= 3 && "Vertex count must be at least 3");
+
+    VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+    seDevice.createBuffer(
+        bufferSize,
+        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        vertexBuffer,
+        vertexBufferMemory);
+
+    void* data;
+    vkMapMemory(seDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+    vkUnmapMemory(seDevice.device(), vertexBufferMemory);
+}
+
+void SeModel::bind(VkCommandBuffer commandBuffer) {
+    VkBuffer buffers[] = {vertexBuffer};
+    VkDeviceSize offsets[] = {0};
+    vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+}
+
+void SeModel::draw(VkCommandBuffer commandBuffer) {
+    vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+}
+
+std::vector<VkVertexInputBindingDescription> SeModel::Vertex::getBindingDescriptions(){
+    std::vector<VkVertexInputBindingDescription> bindingDescriptions(1);
+    bindingDescriptions[0].binding = 0;
+    bindingDescriptions[0].stride = sizeof(Vertex);
+    bindingDescriptions[0].inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+    return bindingDescriptions;
+}
+
+std::vector<VkVertexInputAttributeDescription> SeModel::Vertex::getAttributeDescriptions() {
+    std::vector<VkVertexInputAttributeDescription> attributeDescriptions(1);
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[0].offset = 0;
+    return attributeDescriptions;
+}
+}
---
+++ engine/se_model.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "se_device.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+#include <vector>
+
+namespace se {
+
+class SeModel {
+public:
+    struct Vertex {
+        glm::vec2 position;
+
+        static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+        static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+    };
+
+    SeModel(SeDevice& device, const std::vector<Vertex>& vertices);
+    ~SeModel();
+
+    SeModel(const SeModel &) = delete;
+    SeModel &operator= (const SeModel &) = delete;
+
+    void bind(VkCommandBuffer commandBuffer);
+    void draw(VkCommandBuffer commandBuffer);
+
+private:
+    void createVertexBuffers(const std::vector<Vertex>& vertices);
+
+    SeDevice& seDevice;
+    VkBuffer vertexBuffer;
+    VkDeviceMemory vertexBufferMemory;
+    uint32_t vertexCount;
+};
+
+}
+
+
